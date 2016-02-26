e9632a002634756516a47b08e3a4cc7b82e1e2c2
James Smyth <smythie_89@hotmail.com>
2016-02-26 16:54:36 +0000

    started a harvest time bar that tells the user when to plant their seeds

--- modules/FoodItem.js
+++ modules/FoodItem.js
@@ -9,20 +9,29 @@ class FoodItem extends React.Component {
     render () {
         var FoodItem  = this.props.state;
         var foodParam = this.props.param;
-        console.log(FoodItem, foodParam);
+
+        // map function to loop through each object item and get the key and value.
+        var list = Object.keys(FoodItem[foodParam].monthsActive).map(function(month) {
+
+            // month returns 'January true'
+            // FoodItem[foodParam].monthsActive[month] returns the value true or false
+            console.log(month, FoodItem[foodParam].monthsActive[month]);
+
+            // 'inSeason' is either true or false
+            var inSeason = FoodItem[foodParam].monthsActive[month];
+            var type = inSeason == true ? 'in-season' : 'not-in-season';
+
+            return <li key={month} className={type}>{month}</li>
+        });
 
         return (
-            <div className={currentMonth(FoodItem[foodParam].monthsActive)}>
-                <h2>{FoodItem[foodParam].name}</h2>
+            <div>
+                <h1>{FoodItem[foodParam].name}</h1>
                 <p>Color: {FoodItem[foodParam].color}</p>
                 <p>Description: {FoodItem[foodParam].description}</p>
-                <p>Months active:
-                    {
-                        FoodItem[foodParam].monthsActive.map(function(month){
-                            return transformMonth(month)
-                        })
-                    }
-                </p>
+                <ul>
+                    {list}
+                </ul>
             </div>
         )
     }
--- store/Data.js
+++ store/Data.js
@@ -1,3 +1,25 @@
+// var Data = {
+//     food: {
+//         apples: {
+//             "name": "apples",
+//             "type": "fruit",
+//             "color": "red",
+//             "description": "Apples are grown in the ground",
+//             "monthsActive": [2, 3, 4, 5, 6, 7]
+//         },
+//         carrots: {
+//             "name": "carrots",
+//             "type": "vegetables",
+//             "color": "orange",
+//             "description": "Carrots are grown in the sky",
+//             "monthsActive": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//         }
+//     }
+// }
+//
+// export default Data
+
+
 var Data = {
     food: {
         apples: {
@@ -5,14 +27,40 @@ var Data = {
             "type": "fruit",
             "color": "red",
             "description": "Apples are grown in the ground",
-            "monthsActive": [2, 3, 4, 5, 6, 7]
+            "monthsActive": {
+                "January": false,
+                "February": false,
+                "March": false,
+                "April": false,
+                "May": false,
+                "June": false,
+                "July": true,
+                "August": true,
+                "September": true,
+                "October": true,
+                "November": true,
+                "December": true
+            }
         },
         carrots: {
             "name": "carrots",
             "type": "vegetables",
             "color": "orange",
             "description": "Carrots are grown in the sky",
-            "monthsActive": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            "monthsActive": {
+                "January": true,
+                "February": true,
+                "March": true,
+                "April": true,
+                "May": true,
+                "June": false,
+                "July": false,
+                "August": false,
+                "September": false,
+                "October": false,
+                "November": false,
+                "December": true
+            }
         }
     }
 }
