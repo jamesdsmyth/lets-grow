47c70223e0109ce1c3cc01189278a13cea7df7d4
James Smyth <smythie_89@hotmail.com>
2016-03-10 13:22:46 +0000

    continued with moment functionality

--- containers/MyGardenContainer.js
+++ containers/MyGardenContainer.js
@@ -2,6 +2,8 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
+import moment from 'moment'
+
 import * as actions from '../actions/action-creators'
 
 class MyGardenContainerView extends React.Component {
@@ -12,22 +14,45 @@ class MyGardenContainerView extends React.Component {
         // I need to create a different list
         var touchedFoodList = Object.keys(AllFood).map(function (key) {
             console.log(AllFood[key].isGrowing)
-
-
-
             if(AllFood[key].isGrowing === true) {
 
-
-                var startedGrowingLive = setTimeout(function () {
-                    actions.updateTime(AllFood[key].startedGrowing)
-                }, 1000);
-
+                var startTime = AllFood[key].startedGrowing;
+                var continuedTime = startTime;
+                var latestTime = null;
 
 
+
+                // didnt even think. I don't even need this. All I need is the start time and a time.
+                // incrementOneSecond();
+                // function incrementOneSecond () {
+                //     setTimeout(function () {
+                //         latestTime = moment(continuedTime).add(1, 'second')//;.format();
+                //         continuedTime = latestTime;
+                //
+                //         console.log(moment(latestTime).format());
+                //         incrementOneSecond();
+                //     }, 1000);
+                // }
+
+                var startCount = 0;
+                var continuedCount = startCount;
+                var latestCount = startCount;
+
+                incrementOneSecond();
+                function incrementOneSecond () {
+                    setTimeout(function () {
+                        latestCount = moment(continuedCount).add(1, 'second')//;.format();
+                        continuedCount = latestCount;
+
+                        console.log(moment(startCount).format());
+                        incrementOneSecond();
+                    }, 1000);
+                }
 
                 return <li key={AllFood[key].name}>
                             <h3>{AllFood[key].name}</h3>
-                            <p>Started growing: {startedGrowingLive}</p>
+                            <p>Started growing at: {startCount}</p>
+                            <p>Has been growing for </p>
                         </li>
             }
         });
