12b7c47a9926a76cd5113be9d7d487ebb1cf4f0a
James Smyth <smythie_89@hotmail.com>
2016-04-05 14:10:26 +0100

    timer now works for when you start growing food

--- containers/MyGardenContainer.js
+++ containers/MyGardenContainer.js
@@ -7,9 +7,28 @@ import moment from 'moment'
 import * as actions from '../actions/action-creators'
 
 class MyGardenContainerView extends React.Component {
-    render () {
-        var AllFood = this.props.AllFood;
+    constructor(props) {
+        super(props);
+        this.state = {
+            allFood: props.AllFood,
+            timer: 0
+        };
+    }
 
+    tick() {
+        this.setState({ timer: this.state.timer + 1000 })
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.tick.bind(this), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render () {
+        var AllFood = this.state.allFood;
         // getting all food items that have started growing.
         var touchedFoodList = Object.keys(AllFood).map(function (key) {
 
@@ -18,24 +37,15 @@ class MyGardenContainerView extends React.Component {
                 var startTime = AllFood[key].startedGrowing;
                 var diff,duration,momentObj,incrementOneSecond;
 
-                var incrementOneSecond = function () {
-                    return setTimeout(function () {
-                        diff = moment().diff(startTime);
-                        duration = moment.duration(diff).humanize();
-                        console.log(diff, duration);
-
-                        incrementOneSecond();
-                        return duration;
-                    }, 1000);
-                }
+                var diff = moment.duration(moment().diff(startTime)).humanize();
 
                 return <li key={AllFood[key].name}>
                             <h3>{AllFood[key].name}</h3>
                             <p>Started growing at: {startTime}</p>
-                            <p>duration is {incrementOneSecond()}</p>
+                            <p>duration is {diff}</p>
                         </li>
             }
-        });
+        }.bind(this));
 
         return (
             <div>
