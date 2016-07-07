d50789f9355a264acb99f94a40919092ec6e8537
James Smyth <smythie_89@hotmail.com>
2016-07-07 15:52:01 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Lets grow
 =====================
 
-Lets grow is an app React, Redux and Webpack that lists how to grow a selection of fruit and vegetables in the garden. It also gives you warnings when to water the growing plants. Please note the warnings are currently set at every 10 seconds but are configurable!
+Lets grow is a React app using Redux and Webpack that lists how to grow a selection of fruit and vegetables in the garden. It also gives you warnings when to water the growing plants. Please note the warnings are currently set at every 10 seconds but are configurable!
 
 ### How to run
 
--- index.js
+++ index.js
@@ -25,25 +25,3 @@ render((
         </Router>
     </Provider>
 ), document.getElementById('lets-grow'));
-
-
-// To do
-
-// - Done - Got the store working using Redux.
-// - Done - List all food items out using the template in FoodItem.js. Get each object out from the store for each individual page
-// - Done - For some reason the foodItem page is not being linked to. Get this all hooked up
-// - Done - Now need to pull in the individual food items instead of all items from within the store. Done by referencing the param in FoodItem.js
-// - Done - Sass needs to be supported
-// - Done - Months are now listed and if current month is one of the months that food grows in, add certain class
-// - Done - Add some way of displaying when exactly the food should be grown through the months. Needs to be visual
-// - Done - Add a grow button that writes to the store. Added the start and stop reducers
-// - Done - List all currently growing plants in the 'my garden' tab
-// - Done - Add moment.js and have readable times being displayed. Added the package, now getting to format the dates
-// - Done - When clicking 'stop growing' within garden, this should refresh the render and remove the item. Could tap into the interval?
-// - Done - Color scheme and theme needs to be thought out and implemented
-// - Done - Add real fruit and veg data
-// - Done - Add in prompts to tell the user when to water the plants. The notications can be custom and will be done every few minutes for demo purposes
-// - Done - Add hover states on the buttons
-// - Add in screenshots
-// - Make fully mobile friendly, needs to fix a few width bugs
-// - Add dropdown to list what is in season and what is not in season
